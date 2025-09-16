2188a6bd7f035c67430c7b558d225a26f99a17dd
Tens1des <romankotov3213@gmail.com>
2025-09-16 23:19:28 +0300

    Initial Commit

---
+++ Whip Master/Whip_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whip_MasterApp.swift
+//  Whip Master
+//
+//  Created by Рома Котов on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Whip_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
